52aa6cbe9d024cecdbf8fb0c60a4faea58558d21
Alessandro La Veglia <a.laveglia@itsrizzoli.it>
2023-07-11 14:24:54 +0200

    Aggiunta la parte in cui si controlla se si e  collegati al server

--- Main.py
+++ Main.py
@@ -1,21 +1,26 @@
 from neo4j import GraphDatabase
 
-#connessione al batabase neo4j
+# Connection to the Neo4j database
 uri = "neo4j+s://ece1266a.databases.neo4j.io"
 user = "neo4j"
 password = "qPxdTyg5M_3K68J_fCWvrm6DAwxMZS8hHVxTbVGSJ94"
 
 driver = GraphDatabase.driver(uri, auth=(user, password))
 
-
-def creazione_nodi():
+def check_connection():
     session = driver.session()
     
+    # Execute a simple query to check the connection
+    result = session.run("RETURN 1")
+    record = result.single()
     
+    if record:
+        print("Connected to Neo4j server.")
+    else:
+        print("Failed to connect to Neo4j server.")
     
-    
-    result = session.run(#query)
-                         
-      #questi servono per chiudere il collegamento al server neo4j                   
-    # session.close()
-    # driver.close()
+    session.close()
+    driver.close()
+
+# Call the function to check the connection
+check_connection()
