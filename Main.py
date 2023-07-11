1a0d18a7a803bc2b90524d8424e89dc2570a9cf8
Alessandro La Veglia <a.laveglia@itsrizzoli.it>
2023-07-11 13:31:29 +0200

    cambiata parte in cui si fa il collegamento

--- Main.py
+++ Main.py
@@ -1,10 +1,21 @@
 from neo4j import GraphDatabase
 
-
-
 #connessione al batabase neo4j
 uri = "neo4j+s://ece1266a.databases.neo4j.io"
 user = "neo4j"
 password = "qPxdTyg5M_3K68J_fCWvrm6DAwxMZS8hHVxTbVGSJ94"
 
-driver = GraphDatabase.driver(uri, auth=(user, password))   
+driver = GraphDatabase.driver(uri, auth=(user, password))
+
+
+def creazione_nodi():
+    session = driver.session()
+    
+    
+    
+    
+    result = session.run(#query)
+                         
+      #questi servono per chiudere il collegamento al server neo4j                   
+    # session.close()
+    # driver.close()
