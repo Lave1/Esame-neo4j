2bc5a02bfe11054fa44099eca1bdc33db1938f03
Leon2052 <leonalbano02@gmail.com>
2023-07-19 14:48:54 +0200

    Ho corretto tre collegamenti

--- Main.py
+++ Main.py
@@ -49,11 +49,11 @@ def create_relationships():
     query_create_relationships = """
     MATCH (p1:peak {name: 'picco 1'}),
           (l:lake {name: 'lago'})
-    CREATE (p1)-[:difficile{distanza:'2km', percorribilità_in_bici: 'no', tempo: '1h 30m'}]->(l)
+    CREATE (p1)-[:facile{distanza:'2km', percorribilità_in_bici: 'no', tempo: '1h 30m'}]->(l)
     """
 
     session.run(query_create_relationships)
-    print("Collegamento 'difficile' tra picco 1 e lago creato con informazioni aggiuntive.")
+    print("Collegamento 'facile' tra picco 1 e lago creato con informazioni aggiuntive.")
 
     query_create_relationships = """
     MATCH (l:lake {name: 'lago'}),
@@ -67,20 +67,20 @@ def create_relationships():
     query_create_relationships = """
     MATCH (p1:peak {name: 'picco 1'}),
           (r1:shelter {name: 'rifugio 1'})
-    CREATE (p1)-[:facile{distanza:'3km', percorribilità_in_bici: 'si', tempo: '2h 30m'}]->(r1)
+    CREATE (p1)-[:difficile{distanza:'3km', percorribilità_in_bici: 'si', tempo: '2h'}]->(r1)
     """
 
     session.run(query_create_relationships)
-    print("Collegamento 'facile' tra picco 1 e rifugio 2 creato con informazioni aggiuntive.")
+    print("Collegamento 'difficile' tra picco 1 e rifugio 1 creato con informazioni aggiuntive.")
 
     query_create_relationships = """
     MATCH (p1:peak {name: 'picco 1'}),
           (p2:peak {name: 'picco 2'})
-    CREATE (p1)-[:difficile{distanza:'6km', percorribilità_in_bici: 'no', tempo: '3h'}]->(p2)
+    CREATE (p1)-[:facile{distanza:'6km', percorribilità_in_bici: 'no', tempo: '3h'}]->(p2)
     """
 
     session.run(query_create_relationships)
-    print("Collegamento 'difficile' tra picco 1 e picco 2 creato con informazioni aggiuntive.")
+    print("Collegamento 'facile' tra picco 1 e picco 2 creato con informazioni aggiuntive.")
 
     query_create_relationships = """
     MATCH (r1:shelter {name: 'rifugio 1'}),
