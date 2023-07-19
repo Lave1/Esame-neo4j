d82a30d499ebffbdcfe802c7d1de60501d9f82b8
Alessandro La Veglia <a.laveglia@itsrizzoli.it>
2023-07-19 10:53:28 +0200

    aggiunto la funzione che crea nodi e fatto il primo

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
-from neo4j import GraphDatabase # Importa la classe GraphDatabase dalla libreria di neo4j
-from Nody import create_nodes_if_not_exists # Importa la funzione crea un nodo se non esiste 
+from neo4j import GraphDatabase
+from Nody import create_nodes_if_not_exists
 
-# Connessione al database di Neo4j
 uri = "neo4j+s://ece1266a.databases.neo4j.io"
 user = "neo4j"
 password = "qPxdTyg5M_3K68J_fCWvrm6DAwxMZS8hHVxTbVGSJ94"
@@ -11,7 +10,6 @@ driver = GraphDatabase.driver(uri, auth=(user, password))
 def check_connection():
     session = driver.session()
     
-    # Esegue una query per controllare le connessioni
     result = session.run("RETURN 1")
     record = result.single()
     
@@ -22,13 +20,24 @@ def check_connection():
     
     session.close()
 
-# Richiama la funzione controlla connessioni
 check_connection()
 
 if __name__ == '__main__':
-
-    # Richiamon la funzione crea un nodo se non esiste
     create_nodes_if_not_exists()
 
-# Chiude la connessione con il drive
+def create_relationships():
+    session = driver.session()
+
+    query_create_relationships = """
+    MATCH (s1:start {name: 'partenza 1'}),
+          (l:lake {name: 'lago'})
+    CREATE (s1)-[:facile{distanza:'2km', percorribilità_in_bici: 'si'}]->(l)
+    """
+    session.run(query_create_relationships)
+    print("Collegamento 'facile' tra partenza 1 e lago creato con informazioni aggiuntive.")
+
+    session.close()
+
+create_relationships()
+
 driver.close()
