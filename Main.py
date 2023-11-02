92bd6d7036cb92d7e589fc1f4646cbf21da32449
Alessandro La Veglia <a.laveglia@itsrizzoli.it>
2023-11-02 08:48:22 +0100

    modificato il collegamento al server

--- Main.py
+++ Main.py
@@ -2,9 +2,9 @@ from neo4j import GraphDatabase
 from Nody import create_nodes_if_not_exists
 
 # Sono le credenziali del server neo4j
-uri = "neo4j+s://ece1266a.databases.neo4j.io"
+uri = "neo4j+s://49d42a7e.databases.neo4j.io"
 user = "neo4j"
-password = "qPxdTyg5M_3K68J_fCWvrm6DAwxMZS8hHVxTbVGSJ94"
+password = "MOIX-jSpaBJYSgdbtvF-Z9zHSs9Bm12ocTt_OFMalbA"
 
 driver = GraphDatabase.driver(uri, auth=(user, password))
 
